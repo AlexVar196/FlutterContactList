6de17a9ea0d75a7268c52e86bd102e7723085ef2
alex varshavsky <ledav88881@gmail.com>
2021-08-19 06:14:23 -0400

    added functionality for displaying a hardcoded list and navigation to contact info page

---
+++ lib/contact.dart
@@ -0,0 +1,36 @@
+class Contact {
+  String firstName;
+  String lastName;
+  String phoneNumber;
+  String email;
+
+  Contact(this.firstName, this.lastName, this.phoneNumber, this.email);
+
+  String getFullName() {
+    return firstName + " " + lastName;
+  }
+
+  String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  String getEmail() {
+    return email;
+  }
+
+  void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+  void setEmail(String email) {
+    this.email = email;
+  }
+}
---
+++ lib/contact_list.dart
@@ -0,0 +1,164 @@
+import 'dart:core';
+import 'package:flutter/material.dart';
+import 'dart:developer';
+import './contact.dart';
+
+class ContactList extends StatefulWidget {
+  ContactList({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _ContactListState createState() => _ContactListState();
+}
+
+class _ContactListState extends State<ContactList> {
+  //var _contactList = <Contact>[];
+
+  List<Contact> getContactsFromMemory() {
+    var contactList = <Contact>[];
+    Contact a = new Contact("Mark", "Bezos", "123123123", "email@email.com");
+    Contact b = new Contact("Eric", "Musk", "123123123", "email@email.com");
+    Contact c = new Contact("George", "Trump", "123123123", "email@email.com");
+    contactList.add(a);
+    contactList.add(b);
+    contactList.add(c);
+    return contactList;
+  }
+
+  Widget getContactListView() {
+    var _contactList = getContactsFromMemory();
+    return ListView.builder(
+        itemCount: _contactList.length * 2,
+        itemBuilder: (context, count) {
+          if (count.isOdd) return Divider();
+          final index = count ~/ 2;
+          return _buildRow(_contactList[index]);
+        });
+  }
+
+  Widget _buildRow(Contact contact) {
+    return ListTile(
+      leading: Container(
+        height: double.infinity,
+        child: IconButton(
+          icon: new Icon(
+            Icons.account_circle,
+            size: 38,
+          ),
+          onPressed: () {
+            String name = contact.getFullName();
+            log("_viewContact $name");
+            _viewContactInfo(contact);
+          },
+        ),
+      ),
+      title: Text(contact.getFullName()),
+      subtitle: Text(contact.phoneNumber),
+      trailing: Container(
+        height: double.infinity,
+        child: IconButton(
+          icon: new Icon(
+            Icons.edit,
+            size: 28,
+          ),
+          onPressed: () {
+            String name = contact.getFullName();
+            log("_editContact $name");
+          },
+        ),
+      ),
+    );
+  }
+
+  String getInitials(String name) => name.isNotEmpty
+      ? name.trim().split(' ').map((l) => l[0]).take(3).join()
+      : '';
+
+  void _viewContactInfo(Contact contact) {
+    String name = contact.getFullName();
+    log("_viewContactInfo for $name");
+
+    Navigator.of(context)
+        .push(MaterialPageRoute(builder: (BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: Text(name)),
+        body: SafeArea(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            CircleAvatar(
+              child: Text(getInitials(name).toUpperCase(),
+                  style: TextStyle(fontSize: 45)),
+              radius: 60.0,
+              backgroundColor: Colors.amber,
+            ),
+            SizedBox(height: 15),
+            Text(
+              name,
+              style: TextStyle(
+                  fontSize: 27.0,
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.amber,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.cyan,
+                ),
+                title: Text(
+                  '+91 1234 567890',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      fontFamily: 'Source Sans Pro',
+                      color: Colors.cyan),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.cyan,
+                ),
+                title: Text(
+                  'keertirajmalik@gmail.com',
+                  style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                      color: Colors.cyan),
+                ),
+              ),
+            ),
+          ]),
+        ),
+      );
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: getContactListView(),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _addContact,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  void _addContact() {
+    log("_addContact");
+  }
+}
